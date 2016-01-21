726ab927675fc240feb318ed807b1f495933be2e
Parag Pardeshi <paragpardeshi@Parags-MacBook-Pro.local>
2016-01-21 15:38:37 -0800

    Added all the files

---
+++ Parag-Showcase/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Parag-Showcase
+//
+//  Created by Parag Pardeshi on 1/21/16.
+//  Copyright © 2016 Parag Pardeshi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
+let SEGUE_LOGGED_IN = "loggedIn"
+let KEY_UID = "uid"
+
+//Firebase Status Codes
+let STATUS_MISSING_CREDENTS = -5
+let STATUS_ACCOUNT_NOEXIST = -8
